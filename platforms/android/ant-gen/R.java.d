32b0766f8557e60896ee594364f2051faf498c75
Claus Bornich <clausb@gmail.com>
2014-05-01 15:36:06 +0200

    android build

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@
 /home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/res/drawable-mdpi/icon.png \
 /home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/res/drawable-ldpi/icon.png \
 /home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/res/xml/config.xml \
+/home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/ant-build/res/drawable/icon.png \
+/home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
+/home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/ant-build/res/drawable-ldpi/icon.png \
 /home/claus/Projects/phonegap/CordovaGalaxyPortal/platforms/android/ant-build/AndroidManifest.xml \
